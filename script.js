b373817847cb1246676c8796be6c4f9ea4336091
Kalina Sojka <kalina.a.sojka@gmail.com>
2021-10-05 14:09:47 +0200

    game function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const computerBtn = document.querySelector('.computer-btn');
 const computerOutput = document.querySelector('.game-result')
+let playerPoints = 0;
+let compPoints = 0;
 
-const options = ['rock', 'paper', 'scissors'];
 
 function computerPlay() {
+    const options = ['rock', 'paper', 'scissors'];
     //computer throws randomly rock paper or scissors
     let randomNumber = Math.floor(Math.random() * options.length)
     let randomOption = options[randomNumber]
@@ -11,32 +13,32 @@ function computerPlay() {
     return randomOption
 }
 
+function game(num){
+    let playerSelection, computerSelection
+    for (let i = 0; i < num; i++){
+        playerSelection = prompt("enter rock, paper or scissors")
+        playerSelection = playerSelection.toLowerCase()
+        computerSelection = computerPlay();
+        console.log(`Round ${i+1}! Player's choice is ${playerSelection} and computer chose ${computerSelection}.`)
+        playRound(playerSelection, computerSelection)
+        console.log(`After this round the score is player: ${playerPoints}, computer ${compPoints}.`)
+    }
+    console.log(`player has ${playerPoints} points and computer has ${compPoints} points.`)
+}
+
+
 function playRound(playerSelection, computerSelection){
-    let playerPoints = 0
-    let compPoints = 0
     if (playerSelection == "scissors" && computerSelection == "paper" || 
                 playerSelection == "paper" && computerSelection == "rock" ||
                 playerSelection == "rock" && computerSelection == "scissors") {
-        playerPoints = 1
+        console.log("player wins!")
+        playerPoints +=1
     }else if (playerSelection == "scissors" && computerSelection == "rock" ||
                 playerSelection == "rock" && computerSelection == "paper" ||
                 playerSelection == "paper" && computerSelection == "scissors") {
-        compPoints = 1
-    }
-    console.log(`player has ${playerPoints} and computer has ${compPoints}`)
+        console.log("computer wins")
+        compPoints += 1
+    }   
 }
 
-const computerSelection = computerPlay();
-console.log(playRound("rock", computerSelection))
-
-// function game(num) {
-//     for (let i = 0; i<=num; i++) {
-//         const playerChoice = prompt();
-//         let computerChoice = computerPlay()
-//         gameRound(computerChoice, playerChoice);
-//         console.log(playerChoice, computerChoice);
-//         console.log(`User: ${userPoints}, computer: ${compPoints}`);
-//     }
-// }
-
-// game(5)
+game(5)
